c443022f8aad583c7b4b04439c514535425d4642
Michael Horgan <michael.g.horgan@gmail.com>
2025-06-06 14:56:49 -0700

    test update

--- test/test.py
+++ test/test.py
@@ -1,12 +1,23 @@
 from pzp import ParserPool
+import subprocess
+import os
+
+devnull = open(os.devnull, 'w')
 
 # Create a parser pool with 2 readers, 2 move processors
-pool = ParserPool()
+pool = ParserPool(outdir='test/output', printOffset=3, nSimultaneous=3)
 
 # Try adding a file to process
-pool.enqueue("test_pool/test.pgn.zst", "test1")
-pool.enqueue("test_pool/test2.pgn.zst", "test2")
-pool.enqueue("test_pool/test3.pgn.zst", "test3")
+with open("test/list.txt") as f:
+    for line in f:
+        url = line.strip()
+        fn = url.split("/")[-1]
+        print(f"\033[0HDownloading {fn}...", end='\r')
+        fn = os.path.join('test', fn)
+        if not os.path.exists(fn):
+            subprocess.call(["curl", "-o", fn, url], stdout=devnull, stderr=devnull) 
+        print("\033[0HFinished downloading".rjust(100), end='\r')
+        pool.enqueue(fn, os.path.basename(fn).split('.')[0])
 
 # Wait for processing to complete
 pool.join()
